7877b1f88d19e13a14a086591594ce61e922b9ae
Nicolas <122683587+nicxlxs@users.noreply.github.com>
2025-11-12 05:09:30 -0500

    feat(fleet): add Fleet routes and named paths for vehicles/devices

--- lib/fleet/routing/fleet_routes.dart
+++ lib/fleet/routing/fleet_routes.dart
@@ -4,9 +4,26 @@ import '../presentation/pages/device_detail_page.dart';
 import '../presentation/pages/vehicle_management_page.dart';
 import '../presentation/pages/vehicle_detail_page.dart';
 
+// fleet_routes.dart
 final List<GoRoute> fleetRoutes = [
-  GoRoute(path: '/fleet/vehicles',        builder: (_, __) => const VehicleManagementPage()),
-  GoRoute(path: '/fleet/vehicles/:id',    builder: (ctx, st) => VehicleDetailPage(id: int.parse(st.pathParameters['id']!))),
-  GoRoute(path: '/fleet/devices',         builder: (_, __) => const DeviceManagementPage()),
-  GoRoute(path: '/fleet/devices/:id',     builder: (ctx, st) => DeviceDetailPage(id: int.parse(st.pathParameters['id']!))),
+  GoRoute(
+    path: '/fleet/vehicles',
+    name: 'fleet-vehicles',
+    builder: (_, __) => const VehicleManagementPage(),
+  ),
+  GoRoute(
+    path: '/fleet/devices',
+    name: 'fleet-devices',
+    builder: (_, __) => const DeviceManagementPage(),
+  ),
+  GoRoute(
+    path: '/fleet/vehicles/:id',
+    name: 'fleet-vehicle-detail',
+    builder: (ctx, st) => VehicleDetailPage(id: int.parse(st.pathParameters['id']!)),
+  ),
+  GoRoute(
+    path: '/fleet/devices/:id',
+    name: 'fleet-device-detail',
+    builder: (ctx, st) => DeviceDetailPage(id: int.parse(st.pathParameters['id']!)),
+  ),
 ];
